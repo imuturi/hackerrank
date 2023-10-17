7d27aa4d8823e2a38d4a25956bfa7a31d625006b
Ian Kinyua Muturi <KEN20549@kcbgroup.com>
2023-10-17 16:26:50 +0300

    First Commit

---
+++ src/main/java/com/midas/hackerrank/HackerrankApplication.java
@@ -0,0 +1,13 @@
+package com.midas.hackerrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerrankApplication.class, args);
+	}
+
+}
